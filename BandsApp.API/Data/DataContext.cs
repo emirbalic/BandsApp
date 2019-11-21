90abbc5d3bbf540dae3e5ba98e8ef42c39321cea
Emir Balic <emirbalic@gmail.com>
2019-11-21 18:17:39 +0100

    Initial Commit

---
+++ BandsApp.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using BandsApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BandsApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options){}
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
