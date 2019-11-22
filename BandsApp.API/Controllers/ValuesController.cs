df0ccd088f262d05986d493e9983dd101bc112cc
Emir Balic <emirbalic@gmail.com>
2019-11-22 23:17:16 +0100

    Connect FE auth to BE

--- BandsApp.API/Controllers/ValuesController.cs
+++ BandsApp.API/Controllers/ValuesController.cs
@@ -20,7 +20,7 @@ namespace BandsApp.API.Controllers
             _context = context;
 
         }
-        // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues() // ActionResult<IEnumerable<string>> 
         {
