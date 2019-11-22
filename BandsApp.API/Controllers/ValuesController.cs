a93364461ea82df4ba4d9bb9cf59504665ec839f
Emir Balic <emirbalic@gmail.com>
2019-11-22 13:30:06 +0100

    End of Security JWT

--- BandsApp.API/Controllers/ValuesController.cs
+++ BandsApp.API/Controllers/ValuesController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BandsApp.API.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace BandsApp.API.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
@@ -33,7 +35,7 @@ namespace BandsApp.API.Controllers
             return Ok(values); 
         }
 
-        // GET api/values/5
+        [AllowAnonymous]
         [HttpGet("{id}")]
         public async Task<IActionResult> GetValue(int id) // ActionResult<string> 
         {
---
+++ BandsApp.API/Dtos/UserForLoginDto.cs
@@ -0,0 +1,8 @@
+namespace BandsApp.API.Dtos
+{
+    public class UserForLoginDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
