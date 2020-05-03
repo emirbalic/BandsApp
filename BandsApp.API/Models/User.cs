832343606c352df63a4e2e653e4eeac18fd9b157
Emir Balic <emirbalic@gmail.com>
2020-05-04 01:17:03 +0200

    Likes Functionality - still buggy

--- BandsApp.API/Models/User.cs
+++ BandsApp.API/Models/User.cs
@@ -25,5 +25,7 @@ namespace BandsApp.API.Models
         public string City { get; set; }
         public string Country { get; set; }
         public ICollection<Photo> Photos { get; set; }
+        public ICollection <Like> Likers { get; set; }
+        public ICollection <Like> Likees { get; set; }
     }
 }
